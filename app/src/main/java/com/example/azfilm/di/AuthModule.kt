f0a8c202c11a11981744106183bcb81317bff657
TeymurMemmedov <teymurmemmedov314@gmail.com>
2024-06-24 13:34:58 +0400

    firebase added

--- app/src/main/java/com/example/azfilm/di/AuthModule.kt
+++ app/src/main/java/com/example/azfilm/di/AuthModule.kt
@@ -1,5 +1,8 @@
 package com.example.azfilm.di
 
+import com.example.azfilm.R
+import com.example.azfilm.utils.SecretsFile
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
@@ -16,5 +19,14 @@ object AuthModule {
     @Singleton
     fun provideFirebaseAuth():FirebaseAuth{
         return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGoogleSignIn():GoogleSignInOptions{
+        return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(SecretsFile.OAUTH_WEB_KEY)
+            .requestEmail()
+            .build()
     }
 }
